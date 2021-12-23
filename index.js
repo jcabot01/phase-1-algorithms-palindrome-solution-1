1078f129be6c616d3e6e293a4c0758bc3b02131f
JonCabot <jfc0053@gmail.com>
2021-12-23 14:13:53 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let revStr = word.split("").reverse().join("") === word
+  return revStr
 }
 
 /* 
   Add your pseudocode here
+
+  Reverse the input string in a helper
 */
 
 /*
@@ -15,6 +19,26 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
